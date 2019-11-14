94da5ee81a5eea0c97964df50c26e3e83efe809a
easywaldoji <easywaldoji@gmail.com>
2019-11-15 00:24:59 +0900

    Practice String Funcion

--- funciton_ex01.py
+++ funciton_ex01.py
@@ -7,4 +7,12 @@ msg = "Life is short"
 print(msg.split())
 
 message = input("input your message \n")
-print("your message is " + message)
+print("your message is " + ' ' + message)
+
+
+name = input('what is your name?')
+print(name.upper())
+print(name.capitalize())
+print(name.count('a'))
+print(name.lower())
+
