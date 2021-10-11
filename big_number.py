aa3e8e4bea58421b884f5d9c30fbeb42c6f10949
easywaldoji <easywaldoji@gmail.com>
2021-10-11 10:16:15 +0900

    큰수의 합산 알고리즘을 직접 인수를 입력받아 계산하도록 수정

--- big_number.py
+++ big_number.py
@@ -1,11 +1,20 @@
-##n, m, k = map(int, input().split())
-n = 5
-m = 8
-k = 3
-#data = list(map(int, input().split()))
-data = [2,4,5,4,6]
+#n, m, k = map(int, input().split())
+n = int(input('숫자의 총 갯수를 입력하세요'))
+m = int(input('숫자의 총합을 계산할 횟수를 입력하세요'))
+k = int(input('특정 요소가 k 번 합산을 초과할 수 없음을 의미하는 수치를 입력하세요'))
+
+#n = 5
+#m = 8
+#k = 3
+
+#data = list(map(int, input().split(',')))
+#data = [2,4,5,4,6]
+##data = []
+data = list(map(int, input('숫자들을 입력하세요!!!').split()))
 
 data.sort();
+##print(data)
+
 first = data[n - 1]
 second = data[n - 2]
 
