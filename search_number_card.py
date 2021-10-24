9a7d32954fc140c9875876d3980d567c76074a91
easywaldoji <easywaldoji@gmail.com>
2021-10-24 18:17:25 +0900

    2중 For문 방식으로 숫자카드게임 수정

--- search_number_card.py
+++ search_number_card.py
@@ -5,11 +5,17 @@ data = list(map(int, input('숫자들을 입력해주세요.').split()))
 
 result = 0
 
+# for i in range(n):
+#     data = list(map(int, input().split()))
+#     print(data)
+#     min_value = min(data)
+#     result = max(result, min_value)
+
 for i in range(n):
     data = list(map(int, input().split()))
-    print(data)
-    min_value = min(data)
+    min_value = 99999
+    for lineNumber in data:
+        min_value = min(min_value, lineNumber)
     result = max(result, min_value)
-
 
 print(result)
