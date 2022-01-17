81dfefe9664eb1d69233b116a6de8b42d7c33ddc
easywaldo <easywaldoji@gmail.com>
2022-01-18 00:14:58 +0900

    Getter 활용

--- encapsulation_sample.py
+++ encapsulation_sample.py
@@ -11,13 +11,27 @@ class Robot:
     def cal_add(self, a, b):
         return a + b
     
+    def getAge(self):
+        return self.__age
+    
     
     @classmethod
     def how_many(cls):
         print(f"we have {cls.population} robots.")
         print(cls)
+        
+class Siri(Robot):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.__age = age + 10
     
 robot1 = Robot('irobot', 42)
-print(robot1.age)
+print(robot1.getAge())
 
-robot1.age = 10000
+
+robotSiri = Siri('mysiri', 20)
+
+
+
+##robot1.age = 10000
+print(robotSiri.getAge())
