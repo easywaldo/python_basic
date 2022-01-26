bd398f9f0c143d04724e2e3013a61f22d8843cb6
easywaldo <easywaldoji@gmail.com>
2022-01-26 15:36:49 +0900

    제네릭 클래스 상속 적용

--- generic_type.py
+++ generic_type.py
@@ -16,12 +16,25 @@ class Robot(Generic[ARM, HEAD]):
     def __str__(self):
         return f"{self.arm} && {self.head}"
         
+
+
 robot: Robot = Robot[int, int](122432, 343534)
 robot2: Robot = Robot[str, str]("2323544", "84539232")
 robot3: Robot = Robot[float, str](345483.135, "249308543")
 
+class Siri(Robot[ARM, HEAD]):
+    pass
+        
+
+siri1 = Siri[int, int](342323, 9238235)
+siri2 = Siri[str, int]("9999", 245324)
+siri3 = Siri[float, str](230845.4523, "12948")
 
 
 print(robot)
 print(robot2)
 print(robot3)
+
+print(siri1)
+print(siri2)
+print(siri3)
