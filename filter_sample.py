e25526cc5f2ce73303ac1e55026192bb12bde4dc
easywaldo <easywaldoji@gmail.com>
2022-01-30 22:25:28 +0900

    Filter function sample

---
+++ filter_sample.py
@@ -0,0 +1,6 @@
+result = list(filter(lambda x: x > 100, [200, 300, 100, 99, 400, 900, 1000]))
+print(result)
+
+
+result = list(filter(lambda x: len(x) > 5, ['apple', 'macbook', 'airpods', 'water melon']))
+print(result)
