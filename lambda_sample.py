223b08588e3122a809b60ce545511569adf8b667
easywaldo <easywaldoji@gmail.com>
2022-01-30 21:59:42 +0900

    Lambda function sample

---
+++ lambda_sample.py
@@ -0,0 +1,8 @@
+print((lambda x: x + 1)(10))
+plusAddOne = (lambda x: x + 1)
+print(plusAddOne(10))
+
+
+compareTen = lambda a: True if a > 10 else False
+print(compareTen(11))
+print(compareTen(5))
