e88635e5c1e51cbe2fea49ba2434eae8ba071dc1
easywaldo <easywaldoji@gmail.com>
2022-02-03 01:15:29 +0900

    File read using with context

--- file_object_read.py
+++ file_object_read.py
@@ -4,3 +4,9 @@ file = open("sample.txt", "rb")
 data = pickle.load(file)
 print(data)
 file.close()
+
+
+with open('data.txt', 'r', encoding='utf-8') as file:
+    data = file.read()
+    print(f'data : {data}')
+    file.close()
