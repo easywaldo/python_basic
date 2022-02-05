2b7c4a728d1c7b3b27e467ad558896cda53df553
easywaldo <easywaldoji@gmail.com>
2022-02-05 20:55:48 +0900

    Name mangling

---
+++ name_mangling.py
@@ -0,0 +1,23 @@
+import abc
+
+class Unit():
+    count = 0
+    def __init__(self, hp, name, value):
+        self.__hp = 100
+        self.name = name
+        self.value = value
+        Unit.count += 1
+    @property
+    def hp(self):
+        return self.__hp
+    def minus(self):
+        self.__hp -= 1
+    
+
+myUnit = Unit(100, "robot", "waldo")
+## getter access
+print(myUnit.hp)
+
+## name mangling
+myUnit._Unit__hp = 10000
+print(myUnit.hp)
