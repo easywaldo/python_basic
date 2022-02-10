db22294699343d6ae77afb5a887fe65bc027e99f
easywaldo <easywaldoji@gmail.com>
2022-02-11 00:17:25 +0900

    Closure 에 대한 속성 접근

--- closer.py
+++ closer.py
@@ -6,3 +6,23 @@ def outer(x):
 func = outer(10)
 print(func)
 
+
+def greeting(name, age, gender):
+    def inner():
+        print(f'{name} 님 안녕하세요')
+        print(f'나이 {age}')
+        print(f'성별 {gender}')
+    return inner
+
+closure = greeting('waldo', 42, 'male')
+closure()
+
+print(dir(closure))
+print(dir(closure.__closure__))
+print(dir(closure.__closure__[0]))
+print(closure.__closure__[0].cell_contents)
+print(closure.__closure__[1].cell_contents)
+print(closure.__closure__[2].cell_contents)
+
+for i in closure.__closure__:
+    print(i.cell_contents)
