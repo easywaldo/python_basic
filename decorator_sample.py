ec0b1322ccabd794ebde798fbe545d00f9994725
easywaldo <easywaldoji@gmail.com>
2022-02-11 16:55:20 +0900

    Decorator sample

---
+++ decorator_sample.py
@@ -0,0 +1,12 @@
+def logger(func):
+    def wrapper(arg):
+        print("func start")
+        func(arg)
+        print("func finished")
+    return wrapper
+
+@logger
+def print_hello(name):
+    print(f"hello {name} !!")
+    
+print_hello('waldo')
