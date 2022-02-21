5df0c29c3058633d15dee68dfba0e19e3b58b987
jinam.lee <jinam.lee@a-bly.com>
2022-02-21 15:40:00 +0900

    List extend and append sample

--- list.py
+++ list.py
@@ -1,7 +1,15 @@
-list = [1,3,4,5,6,767,100]
+list = [1, 3, 4, 5, 6, 767, 100]
 print(list)
 list.sort()
 print(list)
 
 for n in list:
     print(n)
+
+list_test = [(1, "waldo"), (2, "ted"), (3, "bob")]
+
+list_test.append((4, "leon"))
+
+list_test.extend([(5, "nix"), (6, "james")])
+
+print(list_test)
