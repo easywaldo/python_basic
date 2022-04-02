0ba737f0d348c28230cd2676cabff2f694a8434c
easywaldo <easywaldoji@gmail.com>
2022-04-03 00:15:11 +0900

    User instance validation

--- pydantic_sample_01.py
+++ pydantic_sample_01.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from typing import List, Optional
 from pydantic import BaseModel
+from pydantic import ValidationError
 
 
 class User(BaseModel):
     id: int
-    name = 'John Doe'
+    name: str = 'waldo'
     signup_ts: Optional[datetime] = None
     friends: List[int] = []
 
@@ -15,9 +16,19 @@ external_data = {
     'signup_ts': '2019-06-01 12:22',
     'friends': [1, 2, '3'],
 }
+
 user = User(**external_data)
+
 
 print(user.id)
 print(repr(user.signup_ts))
 print(user.friends)
 print(user.dict())
+
+print(user.name)
+
+
+try:
+    User(signup_ts='broken', friends=[1, 2, 'not number'])
+except ValidationError as e:
+    print(e.json())
