e1a3a3e13a3ca876ca4ae23c948e1d98bb93f482
easywaldo <easywaldoji@gmail.com>
2022-04-14 01:17:46 +0900

    setdefault and defaultdict sample

---
+++ setdefault_sample.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+
+source = (('k1', 'val1'),
+          ('k1', 'val2'),
+          ('k2', 'val3'),
+          ('k2', 'val4'),
+          ('k2', 'val5'))
+
+new_dict1 = {}
+new_dict2 = {}
+new_dict3 = defaultdict(list)
+
+for k, v in source:
+    if k in new_dict1:
+        new_dict1[k].append(v)
+    else:
+        new_dict1[k] = [v]
+
+print(new_dict1)
+
+for k, v in source:
+    new_dict2.setdefault(k, []).append(v)
+
+print(new_dict2)
+
+
+for k, v in source:
+    new_dict3[k].append(v)
+
+print(new_dict3)
