adf0b8748827247079ebebdd16b492cdcd207000
easywaldo <easywaldoji@gmail.com>
2022-05-16 23:57:36 +0900

    class inheritnace sample added

---
+++ class_inhertance.py
@@ -0,0 +1,16 @@
+class Father:
+    def run(self):
+        print('so fast!!')
+
+
+class Son(Father):
+    def jump(self):
+        print('so high')
+
+
+def main():
+    s = Son()
+    s.run()
+    s.jump()
+
+main()
