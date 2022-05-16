b209d8a903b74406e17ce95634bcfe9a954b6f95
easywaldo <easywaldoji@gmail.com>
2022-05-17 00:13:28 +0900

    class __init__ method overriding

--- class_inhertance.py
+++ class_inhertance.py
@@ -16,11 +16,51 @@ class Son(Father, Mother):
         print('so deep, son style')
 
 
+class Car:
+    def __init__(self, id, f):
+        self.id = id
+        self.fuel = f
+
+    def drive(self):
+        self.fuel -= 10
+
+    def add_fuel(self, f):
+        self.fuel += f
+
+    def show_info(self):
+        print('id:', self.id)
+        print('fuel:', self.fuel)
+
+
+class Truck(Car):
+    def __init__(self, id, f, c):
+        super().__init__(id, f)
+        self.cargo = c
+
+    def add_cargo(self, c):
+        self.cargo += c
+
+    def show_info(self):
+        super().show_info()
+        print('cargo:', self.cargo)
+
+
 def main():
     s = Son()
     s.run()
     s.jump()
     s.dive()
+
+    c = Car('32러5234', 0)
+    c.add_fuel(100)
+    c.drive()
+    c.show_info()
+
+    t = Truck('42러 5959', 0, 0)
+    t.add_fuel(100)
+    t.add_cargo(50)
+    t.drive()
+    t.show_info()
 
 
 main()
