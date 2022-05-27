3affee9a9d68977fb4f1a7635bc472022fea40fe
easywaldo <easywaldoji@gmail.com>
2022-05-28 00:31:27 +0900

    property decorator sample

--- property_sample_01.py
+++ property_sample_01.py
@@ -1,6 +1,7 @@
 class Natural:
-    def __init__(self, n):
+    def __init__(self, n, z=0):
         self.setn(n)
+        self.z = z
 
     def getn(self):
         return self.__n
@@ -13,11 +14,22 @@ class Natural:
 
     n = property(getn, setn)
 
+    @property
+    def z(self):
+        return self.__z
+
+    @z.setter
+    def z(self, z):
+        if (z < 1):
+            self.__z = 1
+        else:
+            self.__z = z
+
 
 def main():
     n1 = Natural(1)
     n2 = Natural(2)
-    n3 = Natural(3)
+    n3 = Natural(3, 1)
     n1.n = n2.n + n3.n
     print(n1.n)
 
@@ -25,6 +37,10 @@ def main():
     n1.__dict__['_Natural__n'] = 100
     print(n1.__dict__)
     print(n1.n)
+
+    n3.z = 99
+    n3.z += 1
+    print(n3.z)
 
 
 if __name__ == '__main__':
