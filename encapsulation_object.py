ad02956eab57cf01b3edd674ff02ba722d722a22
easywaldo <easywaldoji@gmail.com>
2022-05-27 23:56:59 +0900

    dict 를 통한 객체 수정

--- encapsulation_object.py
+++ encapsulation_object.py
@@ -1,28 +1,16 @@
 class Person:
     def __init__(self, n, a):
-<<<<<<< HEAD
         self.__name = n
         self.__age = a
-=======
-        self.__name__ = n
-        self.__age__ = a
->>>>>>> 324c1d5119edd4aa6511fc48b564ed481e04062d
 
     def add_age(self, a):
         if(a < 0):
             print('age error')
         else:
-<<<<<<< HEAD
             self.__age += a
 
     def __str__(self):
         return f'name: {self.__name}, age: {self.__age}'
-=======
-            self.__age__ += a
-
-    def __str__(self):
-        return f'name: {self.__name__}, age: {self.__age__}'
->>>>>>> 324c1d5119edd4aa6511fc48b564ed481e04062d
 
 
 def main():
@@ -30,11 +18,16 @@ def main():
     # p.__age += 1
     p.add_age(1)
     print(p)
-<<<<<<< HEAD
     print(p.__dict__)
-=======
->>>>>>> 324c1d5119edd4aa6511fc48b564ed481e04062d
+    p.address = "Seoul"
+    p.nation = "South Korea"
+    print(p)
+    print(p.__dict__)
 
+    p.__dict__['first'] = "Python"
+    p.__dict__['second'] = "Java"
+    print(p)
+    print(p.__dict__)
 
 if __name__ == '__main__':
     main()
