6eeca48f791509fd003618b8be5a07c39bba1d18
easywaldo <easywaldoji@gmail.com>
2022-05-29 14:09:41 +0900

    nested function closer

---
+++ nested_function_closer.py
@@ -0,0 +1,18 @@
+def maker(m):
+    def inner(n):
+        return m * n
+    return inner
+
+
+def main():
+    maker()
+
+
+if __name__ == '__main__':
+    f1 = maker(2)
+    f2 = maker(3)
+
+    print(f1)
+    print(f2)
+    print(f1(2))
+    print(f2(3))
